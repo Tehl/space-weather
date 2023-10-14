d9691b19b3693162bb512acfad9e21c77cf9f28c
Tehl <Tehl@users.noreply.github.com>
2023-10-14 12:11:54 +0100

    Scaffold API project

---
+++ src/SpaceWeather.Api/Controllers/MagneticIndexReadingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpaceWeather.Domain.Context;
+
+namespace SpaceWeather.Api.Controllers;
+
+[Route("MagneticIndexReadings")]
+public class MagneticIndexReadingController : Controller
+{
+    private readonly SpaceWeatherDbContext _dbContext;
+
+    public MagneticIndexReadingController(
+        SpaceWeatherDbContext dbContext
+    )
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var results = await _dbContext.MagneticIndexReadings.ToListAsync();
+        return Ok(results);
+    }
+}
---
+++ src/SpaceWeather.Api/Program.cs
@@ -0,0 +1,24 @@
+﻿using SpaceWeather.Domain.IoC;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSpaceWeatherDbContext("SpaceWeather");
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
