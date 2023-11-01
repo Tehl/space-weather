58de92a0106fa49cfc5d73c57a90aeed4879a533
Tehl <Tehl@users.noreply.github.com>
2023-11-01 18:35:15 +0000

    Scaffold web ui

---
+++ ui/src/main.js
@@ -0,0 +1,10 @@
+import { DateTime } from "luxon";
+import Highcharts from "highcharts";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Ready");
+});
